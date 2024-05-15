794097df918515dbc57979c37e43a427dd933365
andrii26595 <lukiianchuk.andrii@chnu.edu.ua>
2024-05-15 18:29:53 +0300

    Update group.rb

--- five/group.rb
+++ five/group.rb
@@ -1,24 +1,22 @@
-#Написати клас Group (кожна група визначається назвою та списком студентів цієї групи). 
-#Робити перевірку при заданні назви групи (аналогічно до класу Student)
+require_relative 'validation'
+
 class Group
+  include Validation
+
   attr_accessor :name, :students
 
-  def initialize(name)
-    @name = name
-    @students = []
-  end
+    def initialize(name)
+      @name = name
+      @students = []
+      Validation.validate_name(@name)
+    end
+
 
   def add_student(student)
     @students << student
   end
   
-  def validate_name
-    raise ArgumentError, "Неправильна назва групи" unless Validation.match(/^[A-Za-z0-9\s'’]+$/, @name)
-  end
-  
   def sort_students
     @students.sort_by! { |student| [student.surname, student.name] }
   end
-
-
 end
