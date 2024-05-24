fcbeaa12b5bd99f57b17851472a7fea49c67bbe2
andrii26595 <lukiianchuk.andrii@chnu.edu.ua>
2024-05-24 04:14:44 +0300

    Update main.rb

--- five/main.rb
+++ five/main.rb
@@ -13,12 +13,14 @@
 #відсортувати студентів групи в алфавітному порядку 
 #(спочатку прізвище, потім ім'я)
 
+
 require_relative 'student'
 require_relative 'group'
 require 'date'
 
+# Your main program here
 
-#Пошукати студентів по заліковці, 
+# Пошукати студентів по заліковці
 def find_student_by_id_in_groups(groups, student_id)
   groups.each do |group|
     group.students.each do |student|
@@ -28,16 +30,14 @@ def find_student_by_id_in_groups(groups, student_id)
   nil
 end
 
-
-#В головній програмі створити 5 студентів (спробувати створювати з невалідними значеннями),
+# В головній програмі створити 5 студентів (спробувати створювати з невалідними значеннями)
 student1 = Student.new("Ivan", "Ivanov", Date.new(1995, 7, 15), "12345678")
 student2 = Student.new("Maria", "Marin", Date.new(1994, 9, 20), "87654321")
 student3 = Student.new("Kolya", "Poroshenko", Date.new(1990, 5, 10), "99999999")
 student4 = Student.new("Anna", "Illich", Date.new(1993, 3, 25), "77777777")
 student5 = Student.new("Miha", "Ylo", Date.new(1992, 11, 5), "55555555")
 
-
-#Створити дві групи, розділити студентів по групах. 
+# Створити дві групи, розділити студентів по групах
 group1 = Group.new("group1")
 group2 = Group.new("group2")
 
@@ -47,19 +47,16 @@ group1.add_student(student3)
 group2.add_student(student4)
 group2.add_student(student5)
 
-
-
-#відсортувати студентів групи в алфавітному порядку 
+# Відсортувати студентів групи в алфавітному порядку (спочатку прізвище, потім ім'я)
 group1.sort_students
 group2.sort_students
 
-#(спочатку прізвище, потім ім'я)
 puts "Students in Group 1:"
 group1.students.each { |student| puts "#{student.surname}, #{student.name}" }
 puts "\nStudents in Group 2:"
 group2.students.each { |student| puts "#{student.surname}, #{student.name}" }
 
-#Пошукати студентів по заліковці, 
+# Пошукати студентів по заліковці
 student_id_to_find = "12345678"
 student_found = find_student_by_id_in_groups([group1, group2], student_id_to_find)
 if student_found
