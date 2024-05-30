6fe9ee903c1b845f2a625ef00cc9df7abbfb2065
andrii26595 <lukiianchuk.andrii@chnu.edu.ua>
2024-05-30 05:19:00 +0300

    Add files via upload

---
+++ six/app/controllers/projects_controller.rb
@@ -0,0 +1,62 @@
+# app/controllers/projects_controller.rb
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = current_user.projects
+  end
+
+  def show
+  end
+
+  def new
+    @project = current_user.projects.build
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_url, notice: 'Project was successfully destroyed.'
+  end
+
+  def invite
+    @project = Project.find(params[:id])
+    @user = User.find_by(email: params[:email])
+    if @user && !@project.collaborators.include?(@user)
+      @project.collaborators << @user
+      redirect_to @project, notice: 'User invited successfully.'
+    else
+      redirect_to @project, alert: 'Failed to invite user.'
+    end
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+end
