ad0ff21a6e7e058456df89c1497d303bf00ec4a2
RominaMorar <106281330+RominaMorar@users.noreply.github.com>
2024-05-31 12:55:32 +0300

    Delete app directory

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-# Assuming you have a UserMailer configured in your Rails application
-class UserMailer < ApplicationMailer
-    def update_notification(user)
-      @user = user
-      mail(to: @user.email, subject: 'Your account has been updated!')
-    end
-  end
-  
--- app/models/todo.rb
+++
@@ -1,5 +0,0 @@
-class Todo < ApplicationRecord
-  belongs_to :user
-  has_many :project_tasks
-  has_many :projects, through: :project_tasks
-end
