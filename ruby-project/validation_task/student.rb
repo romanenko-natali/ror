5c7c8b14023d0bca604e1569a8044a18d6c6c0d7
vitluchko <98816838+vitluchko@users.noreply.github.com>
2024-06-02 21:13:10 +0300

    Update student.rb

--- ruby-project/validation_task/student.rb
+++ ruby-project/validation_task/student.rb
@@ -4,35 +4,21 @@ require_relative 'validation'
 require 'date'
 
 class Student
-  include Validation
+  attr_reader :first_name, :last_name, :dob, :student_id
 
-  attr_accessor :first_name, :last_name, :birthdate, :student_id
-
-  NAME_PATTERN = /^[A-Z][a-z]+$/
-  ID_PATTERN = /^\d{6}$/
-  MIN_AGE = 16
-  MAX_AGE = 100
-
-  def initialize(first_name, last_name, birthdate, student_id)
-    raise "Invalid first name" unless Validation.match(NAME_PATTERN, first_name)
-    raise "Invalid last name" unless Validation.match(NAME_PATTERN, last_name)
-    raise "Invalid birthdate" unless valid_birthdate?(birthdate)
-    raise "Invalid student ID" unless Validation.match(ID_PATTERN, student_id)
+  def initialize(first_name, last_name, dob, student_id)
+    raise "Invalid first name" unless Validation.match(/^[a-zA-Z]+$/, first_name)
+    raise "Invalid last name" unless Validation.match(/^[a-zA-Z]+$/, last_name)
+    raise "Invalid date of birth" unless Validation.dateInRange('1900-01-01', '2023-12-31', dob)
+    raise "Invalid student ID" unless Validation.match(/^\d{5}$/, student_id)
 
     @first_name = first_name
     @last_name = last_name
-    @birthdate = birthdate
+    @dob = dob
     @student_id = student_id
   end
 
-  private
-
-  def valid_birthdate?(birthdate)
-    return false unless birthdate.is_a?(Date)
-    
-    today = Date.today
-    min_date = today.prev_year(MAX_AGE)
-    max_date = today.prev_year(MIN_AGE)
-    Validation.date_in_range(min_date, max_date, birthdate)
+  def to_s
+    "#{last_name}, #{first_name} (DOB: #{dob}, ID: #{student_id})"
   end
 end
